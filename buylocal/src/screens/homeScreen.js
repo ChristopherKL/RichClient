07281c4c3cee7b7e95357bc2b7cce38b1a40bc8d
karl <karl@karl-linux-pc>
2018-07-19 23:42:57 +0200

    notification fix

--- buylocal/src/screens/homeScreen.js
+++ buylocal/src/screens/homeScreen.js
@@ -83,7 +83,7 @@ export class HomeScreen extends Component {
                                 
                                     /* iOS and Android properties */
                                     title: title, // (optional)
-                                    message: "My Notification Message", // (required)
+                                    message: bigText, // (required)
                                     playSound: false, // (optional) default: true
                                     soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
                                 });
